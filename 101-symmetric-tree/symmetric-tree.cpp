19be982225ecaa66e24f1482d117d9cc9cf6b64b
Himanshiarora05 <himanshiarora0505@gmail.com>
2026-07-24 23:10:34 +0530

    Time: 1 ms (3.84%) | Memory: 18.5 MB (34.48%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool helper(TreeNode* p, TreeNode* q){
+        if(p == NULL && q == NULL) return true;
+        if(p == NULL || q == NULL) return false;
+
+        if(p->val != q->val) return false;
+
+        return helper(p->left, q->right) && helper(p->right, q->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return helper(root->left, root->right);
+    }
+};
