6628561a8912567592623694c7dff515190e4ddd
Himanshiarora05 <himanshiarora0505@gmail.com>
2026-08-01 11:47:26 +0530

    Time: 11 ms (5.79%) | Memory: 17.1 MB (73.85%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-
-        if (root == NULL) return ans;
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < n; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-
-                level.push_back(temp->val);
-
-                if (temp->left != NULL)
-                    q.push(temp->left);
-
-                if (temp->right != NULL)
-                    q.push(temp->right);
-            }
-
-            ans.push_back(level);
+    int levels(TreeNode* root) {
+        if (root == NULL)  return 0;
+        return 1 + max(levels(root->left), levels(root->right));
+    }
+    void nthLevel(TreeNode* root, int currLevel, int level, vector<int>& ans) {
+        if (root == NULL)  return;
+        if (currLevel == level) {
+            ans.push_back(root->val);
+            return;
         }
-
-        return ans;
+        nthLevel(root->left, currLevel + 1, level, ans);
+        nthLevel(root->right, currLevel + 1, level, ans);
+    }
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> fans;
+        int n = levels(root);
+        if (root == NULL)
+            return fans;
+        for (int i = 1; i <= n; i++) {
+            vector<int> ans;
+            nthLevel(root, 1, i, ans);
+            fans.push_back(ans);
+        }
+        return fans;
     }
 };
