5e6889e35f48db9479ddcbd80e746742e733b972
Himanshiarora05 <himanshiarora0505@gmail.com>
2026-08-09 23:14:21 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (8.43%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//inorder traversal of BST is always in ascending Order
+    void inorder(TreeNode* root , vector<int>& ans){
+        if(root == NULL) return;
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+    }
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inorder(root,ans);
+        for(int i = 1;i<ans.size();i++){
+            if(ans[i] <= ans[i-1]) return false;
+        }
+        return true;
+    }
+};
